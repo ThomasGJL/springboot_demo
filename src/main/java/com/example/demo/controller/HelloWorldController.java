d6f50385ad911041214e3cde88785d6c4e8bd754
guojialiang <thomas1127@163.com>
2021-09-06 08:48:28 +0800

    add swagger

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -2,8 +2,14 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
+@Api(value = "desc of class")
 @RestController
 public class HelloWorldController {
 	
@@ -11,8 +17,9 @@ public class HelloWorldController {
 	@Value("${content}")
     private String content; 
 	
+	@ApiOperation(value = "desc of method", notes = "")
     @RequestMapping("/hello")
-    public String index() {
-        return content;
+    public String index(@ApiParam(value = "desc of param" , required=true ) @RequestParam String name) {
+		return "Hello " + name + "!";
     }
 }
